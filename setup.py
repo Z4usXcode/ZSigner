7a7be2c556d3526cda088f415d7f9b3beb3da869
Z4usXcode <alikytn55@gmail.com>
2025-11-21 22:39:11 +0300

    Modify setup.py for package name and classifiers
    Updated package name to 'ZSigner' and added classifiers.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='zsigner',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=[
-        # Eğer başka paketlere ihtiyaç varsa buraya ekle
-        # Örnek: 'requests', 'colorama', 'user_agent'
-    ],
-    python_requires='>=3.8',
+    name='ZSigner',               # Paket adı PyPI’de görünecek isim
+    version='0.1.0',              # İlk sürüm numarası
+    packages=find_packages(),      # zsigner klasörünü bulur
+    install_requires=[],           # Eğer başka paketlere bağımlıysa buraya ekle
+    python_requires='>=3.8',      # Python versiyonunu belirt
     description='TikTok X-Bogus ve X-Gnarly imza oluşturucu',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='ZEUS',
     url='https://github.com/Z4usXcode/ZSigner',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
